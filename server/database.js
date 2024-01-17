be1a26803704aff10c917075572d965c226fa800
Charles Cook <charlescook80@outlook.com>
2024-01-17 15:24:33 +0000

    added env file for sensitive info

--- server/database.js
+++ server/database.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
+
 const {Pool} = require('pg');
 
 const pool = new Pool({
     user: "postgres",
-    password: "postgres",
+    password: process.env.POSTGRES_PASS,
     host: "localhost",
     port: 5432
 });
-
 
 module.exports = pool;
